f25a25822bf72023aea1ee75e510a5f334fcbac2
Gloria <gloria.wan.glory@gmail.com>
2023-05-09 22:15:32 -0400

    added the validate_model() helper function to make check if the id given is an integer. Created a get_all_tasks() and get_by_task_id() routes to retrieve tasks.

--- app/models/task.py
+++ app/models/task.py
@@ -5,5 +5,24 @@ class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
-    is_complete = db.Column(db.Boolean, default = False)
     completed_at = db.Column(db.DateTime, default=None)
+    is_complete = db.Column(db.Boolean, default=False)
+
+    @classmethod
+    def from_dict(cls, task_data):
+        new_task = Task(
+            title = task_data["title"],
+            description = task_data["description"]
+            # completed_at = task_data["completed_at"]
+        )
+        return new_task
+    
+    # method that is used on an object - instance of a class 
+    # after the object is created an object
+    def to_dict(self):
+        return {
+            "id":self.task_id,
+            "title":self.title,
+            "description":self.description,
+            "is_complete": self.is_complete
+        }
--- app/routes.py
+++ app/routes.py
@@ -2,8 +2,25 @@ from flask import Blueprint, jsonify, request, abort, make_response
 from app import db
 from app.models.task import Task
 
+# BLUEPRINTS
 task_bp = Blueprint("tasks", __name__, url_prefix = "/tasks")
 
+# HELPER FUNCTIONS
+def validate_model(cls, model_id):
+    # makes sure the data type is an integer
+    try: 
+        model_id = int(model_id)
+    except ValueError:
+        abort(make_response({"message": f"{model_id} is not a valid type. A {(type(model_id))} data type was provided. Must be a valid integer data type."},400))
+    
+    # model_id is confirmed an integer so connect with db
+    # changed Crystal to cls to utilize
+    model = cls.query.get(model_id)
+
+    if not model:
+        abort(make_response({"message:" : f"{cls.__name__} {model_id} does not exist"},404))
+    
+    return model
 # POST METHOD - create a task 
 # - complete_at defaults with null
 # RETURN - 201 CREATED // task: id / title/ description / is complete
@@ -11,11 +28,7 @@ task_bp = Blueprint("tasks", __name__, url_prefix = "/tasks")
 def create_task():
     request_body = request.get_json()
 
-    new_task = Task(
-        title = request_body["title"],
-        description = request_body["description"],
-        completed_at = request_body["completed_at"]
-    )
+    new_task = Task.from_dict(request_body)
 
     db.session.add(new_task)
     db.session.commit()
@@ -38,10 +51,22 @@ def create_task():
 def read_all_tasks():
     tasks_response = []
 
+    tasks = Task.query.all()
+
+    for task in tasks:
+        tasks_response.append(task.to_dict())
     
+    return jsonify(tasks_response)
+
 
 # GET METHOD - read a task by task id
 # RETURN - 200 OK, task : id / title / description / is complete
+@task_bp.route("/<task_id>", methods = ["GET"])
+def read_by_task_id(task_id):
+    task = validate_model(Task, task_id)
+
+    return task.to_dict(), 200
+
 
 # PUT METHOD - update a task by id
 # - update title
