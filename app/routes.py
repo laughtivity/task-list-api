db716f4f1ae02a60eff648b27944e7a07b37d01d
Gloria <gloria.wan.glory@gmail.com>
2023-05-11 19:59:42 -0400

    small refactoring of code using to_dict() and adjusting spacing and notes

--- app/routes.py
+++ app/routes.py
@@ -5,21 +5,18 @@ from app.models.goal import Goal
 from datetime import datetime
 import requests
 
-
 # BLUEPRINTS #
 task_bp = Blueprint("tasks", __name__, url_prefix = "/tasks")
 goal_bp = Blueprint("goals", __name__, url_prefix = "/goals")
 
 # HELPER FUNCTIONS #
 def validate_model(cls, model_id):
-    # makes sure the data type is an integer
     try: 
         model_id = int(model_id)
     except ValueError:
         abort(make_response({"message": f"{model_id} is not a valid type. A {(type(model_id))} data type was provided. Must be a valid integer data type."},400))
     
     model = cls.query.get(model_id)
-
     if not model:
         abort(make_response({"message" : f"{cls.__name__} {model_id} does not exist"},404))
     
@@ -38,11 +35,12 @@ def slack_bot_notification(task):
         "text": "Hello, World Again!"
         }
     response = requests.post(api_url, headers=headers, data=body)
-    
+
+
+# POST METHOD - create task
 @task_bp.route("", methods = ["POST"])
 def create_task():
     request_body = request.get_json()
-
     try:
         new_task = Task.from_dict(request_body)
     except:
@@ -51,14 +49,7 @@ def create_task():
     db.session.add(new_task)
     db.session.commit()
 
-    return jsonify({
-        "task": {
-            "id": new_task.task_id,
-            "title": new_task.title,
-            "description": new_task.description,
-            "is_complete": new_task.is_complete
-            }
-    }), 201
+    return jsonify({"task": new_task.to_dict()}), 201
 
 # GET METHOD - read all task
 @task_bp.route("", methods = ["GET"])
@@ -84,10 +75,7 @@ def get_all_tasks():
 @task_bp.route("/<task_id>", methods = ["GET"])
 def get_by_task_id(task_id):
     task = validate_model(Task, task_id)
-
-    return {
-        "task": task.to_dict()
-        }, 200
+    return {"task": task.to_dict()}, 200
 
 # PUT METHOD - update a task by id
 @task_bp.route("/<task_id>", methods = ["PUT"])
@@ -103,14 +91,7 @@ def update_task(task_id):
 
     db.session.commit()
 
-    return jsonify({
-        "task": {
-            "id": task.task_id,
-            "title": task.title,
-            "description": task.description,
-            "is_complete": task.is_complete
-            }
-        }), 200
+    return jsonify({"task": task.to_dict()}), 200
 
 # DELETE METHOD - delete a task by id
 @task_bp.route("/<task_id>", methods=["DELETE"])
@@ -120,7 +101,7 @@ def delete_task(task_id):
     db.session.delete(task)
     db.session.commit()
 
-    return { "details": f'Task {task_id} "{task.title}" successfully deleted'}
+    return {"details": f'Task {task_id} "{task.title}" successfully deleted'}
 
 
 
@@ -134,10 +115,7 @@ def mark_complete(task_id):
 
     db.session.commit()
     slack_bot_notification(task)
-
-    return {
-        "task": task.to_dict()
-        }, 200
+    return {"task": task.to_dict()}, 200
 
 # PUT METHOD - update is_complete to false
 @task_bp.route("/<task_id>/mark_incomplete", methods = ["PATCH"])
@@ -148,10 +126,7 @@ def mark_incomplete(task_id):
     task.completed_at = None
 
     db.session.commit()
-
-    return {
-        "task": task.to_dict()
-        }, 200
+    return {"task": task.to_dict()}, 200
 
 
 
@@ -167,25 +142,18 @@ def create_goal():
     
     db.session.add(new_goal)
     db.session.commit()
-
-    return jsonify({
-        "goal": {
-            "id": new_goal.goal_id,
-            "title": new_goal.title
-            }
-        }), 201
+    return jsonify({"goal": new_goal.to_dict()}), 201
 
 # GET METHOD - get all goals
 @goal_bp.route("", methods=["GET"])
 def get_all_goals():
     goals_response = []
-
     goals = Goal.query.all()
 
     for goal in goals:
         goals_response.append(goal.to_dict())
 
-# https://www.programiz.com/python-programming/methods/list/sort
+    # https://www.programiz.com/python-programming/methods/list/sort
     sort_query = request.args.get("sort")
     if sort_query == "asc":
         goals_response.sort(key=lambda x: x.get('title'))
@@ -198,32 +166,22 @@ def get_all_goals():
 @goal_bp.route("/<goal_id>", methods = ["GET"])
 def get_by_goal_id(goal_id):
     goal = validate_model(Goal, goal_id)
+    return {"goal": goal.to_dict()}, 200
 
-    return {
-        "goal": goal.to_dict()
-        }, 200
 # PUT METHOD - update goal by goal_id
 @goal_bp.route("/<goal_id>", methods = ["PUT"])
 def update_goal(goal_id):
     goal = validate_model(Goal,goal_id)
 
     request_body = request.get_json()
-
     goal.title = request_body["title"]
 
     db.session.commit()
-
-    return jsonify({
-        "goal": {
-            "id": goal.goal_id,
-            "title": goal.title
-            }
-        }), 200
+    return jsonify({"goal": goal.to_dict()}), 200
 
 # DELETE METHOD - delete a goal by goal_id
 @goal_bp.route("/<goal_id>", methods=["DELETE"])
 def delete_goal(goal_id):
-
     goal = validate_model(Goal, goal_id)
         
     db.session.delete(goal)
@@ -231,42 +189,44 @@ def delete_goal(goal_id):
 
     return { "details": f'Goal {goal_id} "{goal.title}" successfully deleted'}
 
-# POST METHOD - post adds a list of task_ids
+
+
+# POST METHOD - post adds a list of task_ids to a goal
 @goal_bp.route("/<goal_id>/tasks", methods = ["POST"])
 def add_tasks_to_existing_goal(goal_id):
     goal = validate_model(Goal, goal_id)
     
     request_body = request.get_json()
-
     task_ids = request_body["task_ids"]
-    # goal_task_list = goal.tasks
 
-    # print("task_ids", task_ids)
     for id in task_ids:
         task = validate_model(Task,id)
         goal.tasks.append(task)
 
     db.session.commit()
-
     return {
         "id": goal.goal_id,
         "task_ids": task_ids
     }
 
+# GET METHOD - reads all tasks for goal id
 @goal_bp.route("/<goal_id>/tasks", methods = ["GET"])
 def read_all_tasks_for_goal_id(goal_id):
     goal = validate_model(Goal, goal_id)
-
     task_response = []
+    
     for task in goal.tasks:
         task_response.append(task.to_dict())
-    print("print task response: ", task_response)
 
     return jsonify({
         "id": goal.goal_id,
         "title": goal.title,
         "tasks": task_response
     }), 200
+
+
+
+
 
 
 # slack bot task completed notification
